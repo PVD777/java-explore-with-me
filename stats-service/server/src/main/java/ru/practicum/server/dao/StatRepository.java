c89e0671984a7490b76fe08efdc86490aedaff5f
PVD <cheaterpvd@yandex.ru>
2023-07-21 11:49:06 +0300

    first try of statsServer of graduated

--- stats-service/server/src/main/java/ru/practicum/server/dao/StatRepository.java
+++ stats-service/server/src/main/java/ru/practicum/server/dao/StatRepository.java
@@ -10,28 +10,28 @@ import java.util.List;
 
 public interface StatRepository extends JpaRepository<EndpointHit, Integer> {
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, count(DISTINCT(h.ip))) " +
+    @Query("SELECT new ru.practicum.server.model.ViewStats(h.app, h.uri, count(DISTINCT(h.ip))) " +
                     "from EndpointHit as h " +
                     "where ((h.timestamp between :start and :end) and h.uri IN :uris) " +
                     "GROUP BY h.app, h.uri " +
                     "ORDER BY COUNT(h.id) desc")
     List<ViewStats> getViewStatsUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT(DISTINCT(h.ip))) " +
+    @Query("SELECT new ru.practicum.server.model.ViewStats(h.app, h.uri, COUNT(DISTINCT(h.ip))) " +
             "FROM EndpointHit AS h " +
             "WHERE h.timestamp between :start and :end " +
             "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(h.id) DESC")
     List<ViewStats> getAllViewStatsUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT((h.ip))) " +
+    @Query("SELECT new ru.practicum.server.model.ViewStats(h.app, h.uri, COUNT((h.ip))) " +
             "FROM EndpointHit AS h " +
             "WHERE h.timestamp between :start and :end and h.uri IN :uris " +
             "GROUP BY h.app, h.uri " +
             "ORDER BY COUNT(h.id) DESC")
     List<ViewStats> getViewStats(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT((h.ip))) " +
+    @Query("SELECT new ru.practicum.server.model.ViewStats(h.app, h.uri, COUNT((h.ip))) " +
             "FROM EndpointHit AS h " +
             "WHERE h.timestamp between :start and :end " +
             "GROUP BY h.app, h.uri " +
